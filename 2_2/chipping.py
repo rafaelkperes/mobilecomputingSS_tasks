5721aad4b937d30bb95f3053d2686e40ec3565f4
Rafael Koch Peres <Rafael Koch Peres>
2016-04-29 18:34:00 +0200

    Added desync (start sending a character at diferent time, start a bit at the same time) senders - receiver working

--- 2_2/chipping.py
+++ 2_2/chipping.py
@@ -11,12 +11,12 @@ chipping = [[1, 1], [1, -1]]
 # ANALOGIC CONSTANTS
 boundaries = [100, 800]
 factor = 1000
-sleeptime = 0.1
+sleeptime = 1
 
 def strtobin(strg):
 	strbin = bin(int(ba.hexlify(strg), 16))[2:]
 	if len(strbin) == 6:
-                strbin = "0" + strbin
+				strbin = "0" + strbin
 	return strbin
 
 def bintostr(strg):
@@ -27,39 +27,54 @@ def startingcode(sending):
 	return "01" + sending
 
 def code(bits, chipping, num):
-    newbits = []
-    mychipping = chipping[num]
-    for b in bits:
-        if b == "0":
-            newbits.append(-1)
-        else:
-            newbits.append(int(b))
-    chippedbits = []
-    for b in newbits:
-        for col in mychipping:
-            chippedbits.append(b * col)
-    return chippedbits
-    
+	newbits = []
+	mychipping = chipping[num]
+	for b in bits:
+		if b == "0":
+			newbits.append(-1)
+		else:
+			newbits.append(int(b))
+	chippedbits = []
+	for b in newbits:
+		for col in mychipping:
+			chippedbits.append(b * col)
+	return chippedbits
+
+def bitdecode(bits, chipping, num):
+	mychipping = chipping[num]
+	acc = 0
+	for j in range(len(mychipping)):
+		acc += bits[j] * mychipping[j]
+	if acc > 0:
+		return 1
+	else:
+		return 0
+
 def decode(bits, chipping, num):
-    decoded = []
-    mychipping = chipping[num]
-    for i in range(0, len(bits), 2):
-        acc = 0
-        for j in range(len(mychipping)):
-            acc += bits[i + j] * mychipping[j]
-        decoded.append(acc)
-    for i in range(len(decoded)):
-        if decoded[i] > 0:
-            decoded[i] = 1
-        else:
-            decoded[i] = 0
-    return ''.join(map(str, decoded))
+	decoded = []
+	mychipping = chipping[num]
+	for i in range(0, len(bits), 2):
+		decoding = []
+		for j in range(len(mychipping)):
+			decoding.append(bits[i + j])
+		decoded.append(bitdecode(decoding, chipping, num))
+	# for i in range(0, len(bits), 2):
+	# 	acc = 0
+	# 	for j in range(len(mychipping)):
+	# 		acc += bits[i + j] * mychipping[j]
+	# 	decoded.append(acc)
+	# for i in range(len(decoded)):
+	# 	if decoded[i] > 0:
+	# 		decoded[i] = 1
+	# 	else:
+	# 		decoded[i] = 0
+	return ''.join(map(str, decoded))
 
 if __name__ == "__main__":
-    bits = "01001000"
-    print "Coding " + bits + ":"
-    print printcode(bits, chipping, 0)
+	bits = "01001000"
+	print "Coding " + bits + ":"
+	print printcode(bits, chipping, 0)
 
-    chipped = [-2, 0, 2, 0, 0, -2, -2, 0, 2, 0, -2, 0, -2, 0, -2, 0]
-    print "Decoding " + chipped + ":"
+	chipped = [-2, 0, 2, 0, 0, -2, -2, 0, 2, 0, -2, 0, -2, 0, -2, 0]
+	print "Decoding " + chipped + ":"
 	#print decode(chipped, chipping, 0)
--- 2_2/recv.py
+++ 2_2/recv.py
@@ -6,80 +6,80 @@ import chipping as chip
 from chipping import charsize, chipsize, id1, id2, chipping, boundaries, factor, sleeptime
 
 def senderstub(sending, ids):
-    ret = []
-    for c in sending:
-        #asc = chip.startingcode(chip.strtobin(c))
-        asc = chip.strtobin(c)
-        chsending = chip.code(asc, chipping, ids) 
-        for b in chsending:
-            #print "Sender 1: " + str(b)
-            #yield b
-            ret.append(b)
-    ret.reverse()
-    return ret
+	ret = []
+	for c in sending:
+		asc = chip.startingcode(chip.strtobin(c))
+		#asc = chip.strtobin(c)
+		chsending = chip.code(asc, chipping, ids) # returns an array of int
+		for b in chsending:
+			ret.append(b) # change to the LED function
+	ret.reverse()
 
-#ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200)
-
-values = []
-value = 0
-
-character = []
-checked = False 
+	if ids == id2: # DESYNCING SENDER -- comment afterwards
+		ret.append(-1)
+		ret.append(-1)
+	
+	return ret
 
 s1stream = senderstub("hello from sender 1", id1)
 s2stream = senderstub("HELLO FROM SENDER 2", id2)
 
+time.sleep(sleeptime)
+
+s1started = False
+s2started = False
+
+recvreset = False
+
+recved = []
+s1bits = []
+s2bits = []
+
 print "Starting..."
 
 while True:
+	if len(recved) >= chip.chipsize: # keeps last read bit
+		recved = []
+
 	#line = ser.readline()
 	#line = line.rstrip()
-	#lastline = line
-	recved = []
-	for i in range(charsize * chipsize):
-		line = s1stream.pop() + s2stream.pop()
-		#print "Received: " + str(line)
-		recved.append(line)
-		time.sleep(sleeptime)
-	rec1 = chip.decode(recved, chipping, id1)
-	rec2 = chip.decode(recved, chipping, id2)
-	print "Sender 1: " + chip.bintostr("0b" + rec1)
-	print "Sender 2: " + chip.bintostr("0b" + rec2)
-	time.sleep(1)
-	# while (int(time.time() * factor) % int(sleeptime * factor) < (sleeptime * factor) / 2):
-	# 	checked = True
-	# 	# several times per interval
-	# 	line = ser.readline()
-	# 	line = line.rstrip()
-	# 	if abs(int(lastline) - int(line)) > 50:
-	# 		break
-	# 	#print line
-	# 	try:
-	# 		values.append(int(line))
-	# 	except ValueError:
-	# 		pass
+	line = s1stream.pop() + s2stream.pop() # SYNCHRONIZED READ - LINE TO BE MODIFIED
+	print "Received: " + str(line)
 
-	# if checked:
-	# 	checked = False
-	# 	print "In!"
-	# 	# once per interval
-	# 	if values:
-	# 		nparray = np.asarray(values)
-	# 		meanvalue = np.mean(nparray)
-	# 		bitvalue = 0
-	# 		if meanvalue < boundaries[0]:
-	# 			bitvalue = 2
-	# 		elif meanvalue < boundaries[1]:
-	# 			bitvalue = 0
-	# 		else:
-	# 			bitvalue = -2
-	# 		print meanvalue
-	# 		print bitvalue
-	# 		if len(character) < charsize:
-	# 			character.append(bitvalue)
-	# 		else:
-	# 			print character
-	# 			character = []
-	# 	values = []
+	time.sleep(float(sleeptime)/10)
+	recved.append(line)
+	#print recved
+
+	if len(recved) >= chipsize: # ready to decode a bit
+		s1bits.append(chip.bitdecode(recved, chipping, id1))
+		if not s1started:
+			# decode and add to s1
+			if s1bits == [0, 1]:
+				s1started = True
+				s1bits = []
+				print "Sender 1 started."
+			elif len(s1bits) == chip.chipsize: # resets, leaving last read bit
+				s1bits = s1bits[-1:]
+		elif len(s1bits) == charsize:
+			print "Sender 1: " + chip.bintostr("0b" + ''.join(map(str, s1bits)))
+			s1bits = []
+			s1started = False
+			time.sleep(sleeptime)
+
+	if len(recved) >= chipsize: # ready to decode a bit
+		s2bits.append(chip.bitdecode(recved, chipping, id2))
+		if not s2started:
+			# decode and add to s2
+			if s2bits == [0, 1]:
+				s2started = True
+				s2bits = []
+				print "Sender 2 started."
+			elif len(s2bits) == chip.chipsize: # resets, leaving last read bit
+				s2bits = s2bits[-1:]
+		elif len(s2bits) == charsize:
+			print "Sender 2: " + chip.bintostr("0b" + ''.join(map(str, s2bits)))
+			s2bits = []
+			s2started = False
+			time.sleep(sleeptime)
 
 # ser.close()
